764c4a6d74b9bf5aa3400a035c735612590e0c5f
Sergio Hernandez-Acosta <79073281+sergiojhernandezacosta@users.noreply.github.com>
2023-02-08 12:59:37 -0600

    Update mpls_pop.py

--- mpls_pop.py
+++ mpls_pop.py
@@ -1,26 +1,32 @@
 import pandas as pd
 import numpy as np
 
+# Asks user input to select the file and location 
 location = input("CSV Location: ")
 file_name = input("File name: ")    # In this case it is 'data.csv'
 
 file = location + "/" + file_name
 
+# Pandas functions to prep the file
 excelfile = pd.read_csv(file)
 df = pd.DataFrame(excelfile)
 
+# Set x and y axes
 years = df.iloc[:,0]
 population = df.iloc[:,1]
 
+# Plot settings
 import matplotlib.pyplot as plt
 plt.bar(years, population, width=8)
 plt.xlabel("Years")
 plt.ylabel("Population")
 plt.title("Census Numbers: Minneapolis, MN")
 
+# Line of best fit
 z = np.polyfit(years, population, 4)
 p = np.poly1d(z)
-#add trendline to plot
+# Add trendline to plot
 plt.plot(years, p(years), color="red")
 
+# Prints out the equation of the trendline
 print(p)
